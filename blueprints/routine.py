09980727e98d4ae253925d06019626c2041034d0
Stéphane Conq <conqstephane@gmail.com>
2021-02-24 21:00:16 +0100

    corrected routine and changed cron schedule

--- blueprints/routine.py
+++ blueprints/routine.py
@@ -14,7 +14,7 @@ def run():
     for user in users:
         for stock in user['watchlist']:
             funding = get_funding(stock['symbol'])
-            if funding['result'][0]['rate'] >= stock['limit']:
+            if funding['result'][0]['rate'] >= float(stock['limit']):
                 message = f"Value {stock['symbol']} is above limit. Limit was set to {stock['limit']}, and is {funding['result'][0]['rate']} from {funding['result'][0]['time'].split('+')[0].replace('T', ' ')}"
                 send_message(user['telegram'], message)
     return "ok"
